692e6dc956db09ed9ce8af68b086c9a8e045fe09
DenisDov <dovzdenis@gmail.com>
2018-09-17 21:55:35 +0300

    add SDK 24.0.0, remove unused redux-promise

--- app/config/Store.js
+++ app/config/Store.js
@@ -1,11 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
-import promise from 'redux-promise';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import RootReducer from './../reducers';
 
-const middleware = [promise, thunk];
+const middleware = [thunk];
 
 const Store = createStore(RootReducer, {}, composeWithDevTools(applyMiddleware(...middleware)));
 
